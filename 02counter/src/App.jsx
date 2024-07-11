76a550cdd74721f40269696d5af159fc447df0f8
astro-israfil <astro.israfil.fi@gmail.com>
2024-07-11 18:24:36 +0600

    learn useState hook in react js

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  const increaseCount = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  }
+
+  const decreaseCount = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+
+  return (
+    <>
+      <h1>React Chill: Counter App</h1>
+      <h2>Count is : {counter}</h2>
+      <button onClick={increaseCount}>Increase count {counter}</button>
+      <button onClick={decreaseCount}>Decrease count {counter}</button>
+    </>
+  )
+}
+
+export default App
